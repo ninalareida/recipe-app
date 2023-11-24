d17c0574e26a3b1dd7d75fd715312ada680debb9
unknown <nina.lareida@bbzbl-it.ch>
2023-11-24 11:38:23 +0100

    added colors and icons

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -2,7 +2,7 @@ import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { receipt, create, basket } from 'ionicons/icons';
+import { receipt, create, basket, restaurant } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -16,6 +16,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ receipt, create, basket });
+    addIcons({ receipt, create, basket, restaurant });
   }
 }
